281e949369b705ef188a792a2d5b681f5c344e75
Alireza <alirezax69prom@gmail.com>
2023-01-19 23:03:12 +0330

    adding a new controller to handling user registration

--- controller/index.js
+++ controller/index.js
@@ -3,9 +3,11 @@
 const movieController = require('./movies.controller');
 const authenticateController = require('./authenticate.controller');
 const usersController = require('./users.controller');
+const registerController = require('./register.controller');
 
 module.exports = {
     movieController,
     authenticateController,
-    usersController
+    usersController,
+    registerController
 }
